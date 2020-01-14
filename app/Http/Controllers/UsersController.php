f3d724e477210dd1c557eeba5972974e8929220e
wintervt <mila23ster@gmail.com>
2020-01-14 21:00:00 +0000

    finishing dashboard with users profile edit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,12 +4,32 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\Users\UpdateProfileRequest;
 
 class UsersController extends Controller
 {
     public function index()
     {
     	return view('users.index')->with('users', User::all());
+    }
+
+    public function edit()
+    {
+        return view('users.edit')->with('user', auth()->user());
+    }
+
+    public function update(UpdateProfileRequest $request)
+    {
+        $user = auth()->user();
+
+        $user->update([
+            'name' => $request->name,
+            'about' => $request->about
+        ]);
+
+        session()->flash('success', 'User was updated successfully');
+
+        return redirect()->back();
     }
 
     public function makeAdmin(User $user)
@@ -22,4 +42,5 @@ class UsersController extends Controller
 
     	return redirect(route('users.index'));
     }
+
 }
