38efb031dea5d607fd71da789c4dca67c5070378
wintervt <mila23ster@gmail.com>
2020-01-16 21:54:49 +0000

    Seeder and front end basic setup

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Post;
+use App\Category;
+use App\Tag;
 use Illuminate\Database\Seeder;
 
 class PostsTableSeeder extends Seeder
@@ -11,8 +14,71 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
+
+        $category1 = Category::create([
+            'name' => 'News'
+        ]);
+
+        $category2 = Category::create([
+            'name' => 'Marketing'
+        ]);
+
+        $category3 = Category::create([
+            'name' => 'Partnership'
+        ]);
+
         $post1 = Post::create([
+            'title' => 'Lorem ipsum dolor sit amet',
+            'description' => 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
+            'content' => 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
+            'category_id' => $category1->id,
+            'image' => 'posts/6.jpg'
 
         ]);
+
+        $post2 = Post::create([
+            'title' => 'Lorem ipsum dolor sit amet 2',
+            'description' => 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 2',
+            'content' => 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 2',
+            'category_id' => $category2->id,
+             'image' => 'posts/7.jpg'
+        ]);
+
+        $post3 = Post::create([
+            'title' => 'Lorem ipsum dolor sit amet 3',
+            'description' => 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 3',
+            'content' => 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 3',
+            'category_id' => $category3->id,
+             'image' => 'posts/8.jpg'
+        ]);
+
+        $post4 = Post::create([
+            'title' => 'Lorem ipsum dolor sit amet 4',
+            'description' => 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 4',
+            'content' => 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 4',
+            'category_id' => $category2->id,
+             'image' => 'posts/9.jpg'
+        ]);
+
+        $tag1 = Tag::create([
+            'name' => 'job'
+        ]);
+
+         $tag2 = Tag::create([
+            'name' => 'customers'
+        ]);
+
+         $tag3 = Tag::create([
+            'name' => 'record'
+        ]);
+
+         $post1->tags()->attach([$tag1->id, $tag2->id]);
+
+         $post2->tags()->attach([$tag2->id, $tag3->id]);
+
+         $post3->tags()->attach([$tag1->id, $tag3->id]);
+
+         $post3->tags()->attach([$tag1->id, $tag3->id]);
+
     }
 }
